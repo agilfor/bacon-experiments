b2c0205b5e1f3cff8e9a80d9ad116d539a14441a
Alan Gil Forza <alan.gil.forza@gmail.com>
2026-03-09 17:58:27 +0100

    Fixes for portability

--- iBacon/brain/main.cpp
+++ iBacon/brain/main.cpp
@@ -1,18 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QDir>         // <-- Add this!
 #include "launcher.h"
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     
-    // Inject our custom C++ Launcher into the QML engine as "System"
     Launcher sys;
     engine.rootContext()->setContextProperty("System", &sys);
     
-    // Load your masterpiece
-    engine.load(QUrl::fromLocalFile("/home/$USER/bacon-experiments/iBacon/SpringBoard.qml"));
+    // THE FIX: Dynamically build the path using QDir::homePath()
+    QString qmlPath = QDir::homePath() + "/bacon-experiments/iBacon/SpringBoard.qml";
+    engine.load(QUrl::fromLocalFile(qmlPath));
+    
     if (engine.rootObjects().isEmpty()) return -1;
     
     return app.exec();
---
+++ iBacon/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Master install command for apk
+sudo apk add build-base cmake qt6-qtbase-dev qt6-qtdeclarative-dev sway lisgd libinput foot badwolf libgpiod-utils
