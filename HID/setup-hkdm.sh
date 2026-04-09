86f15c468cdbceb6dc04f551ad7752a4f5615d79
Alan Gil Forza <alan.gil.forza@gmail.com>
2026-04-09 21:04:29 +0200

    Remove hkdm

--- HID/setup-hkdm.sh
+++
@@ -1,5 +0,0 @@
-sudo rm -f /etc/hkdm/config.d/buttons.toml
-sudo cp "$HOME/bacon-experiments/HID/buttons.toml" /etc/hkdm/config.d/
-sudo chown root:root /etc/hkdm/config.d/buttons.toml
-sudo chmod 644 /etc/hkdm/config.d/buttons.toml
-sudo systemctl restart hkdm
